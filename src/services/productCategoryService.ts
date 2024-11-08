486e8005185e756d1c73a2a8794bdf8e9af3babb
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-11-08 21:33:05 +0800

    feat: product category service

---
+++ src/services/productCategoryService.ts
@@ -0,0 +1,72 @@
+import prisma from "../config/database";
+import ResponseError from "../utils/responseError";
+import { ProductCategory } from "@prisma/client";
+
+class ProductCategoryService {
+  async createProductCategory(productCategory: ProductCategory) {
+    return prisma.productCategory.create({
+      data: {
+        ...productCategory,
+      },
+    });
+  }
+
+  async getAllProductCategories() {
+    return prisma.productCategory.findMany();
+  }
+
+  async getProductCategoryById(id: number) {
+    const productCategory = prisma.productCategory.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!productCategory) {
+      throw new ResponseError(404, "Product Category not found");
+    }
+
+    return productCategory;
+  }
+
+  async updateProductCategory(productCategory: ProductCategory) {
+    const existingProductCategory = await prisma.productCategory.findFirst({
+      where: {
+        id: productCategory.id,
+      },
+    });
+
+    if (!existingProductCategory) {
+      throw new ResponseError(404, "Product Category not found");
+    }
+
+    return prisma.productCategory.update({
+      where: {
+        id: productCategory.id,
+      },
+      data: {
+        ...productCategory,
+      },
+    });
+  }
+
+  async deleteProductCategoryById(id: number) {
+    const existingProductCategory = await prisma.productCategory.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!existingProductCategory) {
+      throw new ResponseError(404, "Product Category not found");
+    }
+
+    return prisma.productCategory.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
+
+export default ProductCategoryService;
