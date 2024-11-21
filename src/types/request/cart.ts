23de82392a5b0261c6467cd8c603a4aec9502412
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-11-21 09:12:15 +0800

    feat: cart types

---
+++ src/types/request/cart.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+const CreateCartRequest = z.object({
+  user_id: z.string(),
+  product_id: z.number(),
+  quantity: z.number().min(0),
+  is_checkout: z.boolean().default(false),
+  sub_total_price: z.number(),
+});
+
+const UpdateCartRequest = z.object({
+  user_id: z.string().optional(),
+  product_id: z.number().optional(),
+  quantity: z.number().min(0).optional(),
+  is_checkout: z.boolean().default(false).optional(),
+  sub_total_price: z.number().optional(),
+});
+
+export { CreateCartRequest, UpdateCartRequest };
