d7eace5fd8b23fcb8b3439ed5fc1667743308dc3
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-11-25 18:51:16 +0800

    feat: count endpoint

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,10 @@ import productRoutes from "./routes/productRoutes";
 import reviewRoutes from "./routes/reviewRoutes";
 import cartRoutes from "./routes/cartRoutes";
 import errorHandler from "./middlewares/errorHandler";
-import './types/global/authUser';
+import "./types/global/authUser";
 import multer from "./middlewares/multer";
 import MediaService from "./services/mediaService";
+import prisma from "./config/database";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -28,11 +29,42 @@ app.use("/api/products", productRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/carts", cartRoutes);
 
-app.post("/api/upload", multer.array("images", 10),(req: Request, res: Response) => {
-  const mediaService = new MediaService();
-  const files = req.files as Express.Multer.File[];
-});
+app.post(
+  "/api/upload",
+  multer.array("images", 10),
+  (req: Request, res: Response) => {
+    const mediaService = new MediaService();
+    const files = req.files as Express.Multer.File[];
+  }
+);
 
+app.get("/api/count", async (req: Request, res: Response) => {
+  try {
+    const usersCount = await prisma.user.count();
+    const marketsCount = await prisma.market.count();
+    const merchantsCount = await prisma.merchant.count();
+    const productsCount = await prisma.product.count();
+
+    res.status(200).json({
+      success: true,
+      status_code: 200,
+      message: "Data statistics retrieved successfully",
+      data: [
+        { users: usersCount },
+        { markets: marketsCount },
+        { merchants: merchantsCount },
+        { products: productsCount },
+      ],
+    });
+  } catch (error) {
+    console.error("Error retrieving counts:", error);
+    res.status(500).json({
+      success: false,
+      status_code: 500,
+      message: "Failed to retrieve data statistics",
+    });
+  }
+});
 
 app.use(errorHandler as any);
 
