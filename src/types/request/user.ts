7c85c4ec41128f2e21a8d89b0b6ac2601613ceea
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-11-27 20:08:05 +0800

    fix: update validation data user

--- src/types/request/user.ts
+++ src/types/request/user.ts
@@ -1,17 +1,25 @@
 import { z } from "zod";
 
+// const CreateUserRequest = z.object({
+//   id: z.string(),
+//   full_name: z.string().min(3).nullable(),
+//   username: z.string().min(3),
+//   email: z.string().email(),
+//   password: z.string().min(3).nullable(),
+//   phone: z.string().min(10).nullable(),
+//   role: z.enum(["USER", "ADMIN", "MERCHANT"]).default("USER"),
+//   provider: z.enum(["GOOGLE", "EMAIL"]),
+//   longitude: z.string().nullable(),
+//   latitude: z.string().nullable(),
+//   address: z.string().nullable(),
+// });
+
 const CreateUserRequest = z.object({
   id: z.string(),
-  full_name: z.string().min(3),
-  username: z.string().min(3),
-  email: z.string().email(),
-  password: z.string().min(3).nullable(),
-  phone: z.string().min(10).nullable(),
-  role: z.enum(["USER", "ADMIN", "MERCHANT"]),
-  provider: z.enum(["GOOGLE", "EMAIL"]),
-  longitude: z.string().nullable(),
-  latitude: z.string().nullable(),
-  address: z.string().nullable(),
+  full_name: z.string().min(3, "Fullname harus memiliki minimal 3 karakter."),
+  username: z.string().min(3, "Username harus memiliki minimal 3 karakter."),
+  email: z.string().email("Format email tidak valid."),
+  password: z.string().min(3, "Password harus memiliki minimal 3 karakter."),
 });
 
 const UpdateUserRequest = z.object({
