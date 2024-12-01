29b67d99385f597ac5d740817cd4725f085ab35c
adityamahendrap <ptadityamahendrap@gmail.com>
2024-12-01 17:40:30 +0800

    add review validation

--- src/services/reviewService.ts
+++ src/services/reviewService.ts
@@ -2,6 +2,8 @@ import prisma from "../config/database";
 import ResponseError from "../utils/responseError";
 import { Review } from "@prisma/client";
 import MediaService, { MediaData, MediaType } from "./mediaService";
+import { Checkout } from "../types/request/cart";
+import { CheckoutStatus } from "../types/enum/dbEnum";
 
 class ReviewService {
   private mediaService: MediaService;
@@ -33,6 +35,31 @@ class ReviewService {
 
     if (!existingUser) {
       throw new ResponseError(404, "User not found");
+    }
+
+    // check if user already reviewed the product
+    const existingReview = await prisma.review.findFirst({
+      where: {
+        product_id: review.product_id,
+        user_id: review.user_id,
+      },
+    });
+    if (existingReview) {
+      throw new ResponseError(400, "You already reviewed this product");
+    }
+
+    // check if user already checked out the product
+    const existingCheckouts = await prisma.checkoutDetails.findMany({
+      where: {
+        product_id: review.product_id,
+        checkout: {
+          user_id: review.user_id,
+          status: CheckoutStatus.COMPLETED,
+        },
+      },
+    });
+    if (existingCheckouts.length === 0) {
+      throw new ResponseError(400, "You need to checkout to review this product");
     }
 
     await this.uploadAndSaveMediasIfExist(files, review.id);
